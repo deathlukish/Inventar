561aaf0c535bc4a1a8f27f19422a6cba51662c00
Дмитрий <deathlukish@mail.ru>
2022-06-29 10:04:37 +0300

    refacoring

--- Inventar/ViewModels/MainVindowViewModel.cs
+++ Inventar/ViewModels/MainVindowViewModel.cs
@@ -9,6 +9,7 @@ using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
 using TelegramBotPolling;
+using TelegramPolling;
 
 namespace Inventar.ViewModels
 {
@@ -17,30 +18,10 @@ namespace Inventar.ViewModels
 
         public MainVindowViewModel()
         {
-
-            LoadBot();
+            BotInit.LoadBot(ConfigLoad.LoadConfig().ApiToken.Token);
         }
 
-        private async void LoadBot()
-        {
-            var bot = new TelegramBotClient(ConfigLoad.LoadConfig().ApiToken.Token);
-            var me = await bot.GetMeAsync();
-            using var cts = new CancellationTokenSource();
-            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-            var receiverOptions = new ReceiverOptions()
-            {
-                AllowedUpdates = Array.Empty<UpdateType>(),
-                ThrowPendingUpdates = true,
-            };
-
-            bot.StartReceiving(updateHandler: UpdateHandlers.HandleUpdateAsync,
-                               pollingErrorHandler: UpdateHandlers.PollingErrorHandler,
-                               receiverOptions: receiverOptions,
-                               cancellationToken: cts.Token);
-            // Send cancellation request to stop bot
-          //  cts.Cancel();
-
-        }
+       
 
     }
 }
---
+++ TelegramBotPolling/BotInit.cs
@@ -0,0 +1,31 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using TelegramBotPolling;
+
+namespace TelegramPolling
+{
+    public static class BotInit
+    {
+        public static async void LoadBot(string Token)
+        {
+            var bot = new TelegramBotClient(Token);
+            var me = await bot.GetMeAsync();
+            using var cts = new CancellationTokenSource();
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            var receiverOptions = new ReceiverOptions()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>(),
+                ThrowPendingUpdates = true,
+            };
+
+            bot.StartReceiving(updateHandler: UpdateHandlers.HandleUpdateAsync,
+                               pollingErrorHandler: UpdateHandlers.PollingErrorHandler,
+                               receiverOptions: receiverOptions,
+                               cancellationToken: cts.Token);
+            // Send cancellation request to stop bot
+            //  cts.Cancel();
+
+        }
+    }
+}
