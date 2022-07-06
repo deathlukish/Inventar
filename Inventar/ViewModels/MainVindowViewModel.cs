3689a9f3ef601766987755bf1ee9e0f110de05b3
Дмитрий <deathlukish@mail.ru>
2022-07-06 10:57:00 +0300

    Add test method GetXMLQuery() for generated query

--- Inventar/ViewModels/MainVindowViewModel.cs
+++ Inventar/ViewModels/MainVindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Windows;
+using System.Xml.Linq;
 using Telegram.Bot.Types;
 using TelegramBotPolling;
 using TelegramPolling;
@@ -19,67 +20,59 @@ namespace Inventar.ViewModels
         {
             BotInit bot = new BotInit(ConfigLoad.LoadConfig().ApiToken!.Token!);
             UpdateHandlers.Update += (Message a)=>MessageBox.Show(a.From.ToString());
-            
+
             GetRef();
+            //RetXML();
         }
         /// <summary>
         /// Отправка запроса на сервер и получения результата в виде XML
         /// </summary>
         public async void GetRef()
         {
-            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => { return true; };
-            //HttpWebRequest requestToServerEndpoint =
-            //(HttpWebRequest)WebRequest.Create("https://172.20.88.50:54322/rk7api/v0/xmlinterface.xml");
-
-            //string fileUrl = @"./GetRefData.xml";
-            //requestToServerEndpoint.Method = WebRequestMethods.Http.Post;
-            //requestToServerEndpoint.KeepAlive = true;
-            //requestToServerEndpoint.Credentials = new NetworkCredential("Internet", "UeM5zP");
-            //MemoryStream postDataStream = new MemoryStream();
-            //StreamWriter postDataWriter = new StreamWriter(postDataStream);
-            //FileStream fileStream = new FileStream(fileUrl, FileMode.Open, FileAccess.Read);
-            //byte[] buffer = new byte[1024];
-            //int bytesRead = 0;
-            //while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
-            //{
-            //    postDataStream.Write(buffer, 0, bytesRead);
-            //}
-            //fileStream.Close();
-            //requestToServerEndpoint.ContentLength = postDataStream.Length;
-            //using (Stream s = requestToServerEndpoint.GetRequestStream())
-            //{
-            //    postDataStream.WriteTo(s);
-            //}
-            //postDataStream.Close();
-            //WebResponse response = requestToServerEndpoint.GetResponse();
-            //StreamReader responseReader = new StreamReader(response.GetResponseStream());
-            //string replyFromServer = responseReader.ReadToEnd();
-            //new version
-            var httpClientHandler = new HttpClientHandler();
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            string userName = "Internet";
+            string passwd = "UeM5zP";
+            byte[] authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+            string filePath = @"./GetRefData.xml";
+            string url = "https://172.20.88.50:54322/rk7api/v0/xmlinterface.xml";
+            string resultXML;
             httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
             {
                 return true;
             };
-            var userName = "Internet";
-            var passwd = "UeM5zP";
-            var url = "https://172.20.88.50:54322/rk7api/v0/xmlinterface.xml";
-            using var client = new HttpClient(httpClientHandler);
-            var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
-            var filePath = @"./GetRefData.xml";
-            using (var multipartFormContent = new MultipartFormDataContent())
+            using (var client = new HttpClient(httpClientHandler))
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
-                var fileStreamContent = new StreamContent(System.IO.File.OpenRead(filePath));
-                //fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
-                multipartFormContent.Add(fileStreamContent);
-                var response = await client.PostAsync(url, multipartFormContent);
-                response.EnsureSuccessStatusCode();
-                var a = await response.Content.ReadAsStringAsync();
+                using (var multipartFormContent = new MultipartFormDataContent())
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+                   // var fileStreamContent = new StreamContent(System.IO.File.OpenRead(filePath));
+                    var reqestXML = new StringContent(GetXMLQuery().ToString());
+                    multipartFormContent.Add(reqestXML);
+                    var response = await client.PostAsync(url, multipartFormContent);
+                    response.EnsureSuccessStatusCode();
+                    resultXML = await response.Content.ReadAsStringAsync();
 
+                }
             }
-            
         }
+        /// <summary>
+        /// Метод формирования XML запроса
+        /// </summary>
+        /// <returns></returns>
+        public XDocument GetXMLQuery()
+        {
+            XAttribute Command;
+            //создаем структуру XML-файла
+            XDocument xmlQuery = new XDocument(
+            new XElement("RK7Query", 
+                    new XElement("RK7Command2", Command =
+                    new XAttribute("CMD", "GetRefData"),
+                    new XAttribute("RefName", "RESTAURANTS"),
+                    new XAttribute("PropMask", "items.(Code,AltName,DeviceLicenses)")))
+              );
+           return xmlQuery;
 
+        }
 
     }
 }
