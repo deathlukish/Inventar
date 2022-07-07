5e4eff659e30ad8d27ef0c939f7afdab3e71e696
Дмитрий <deathlukish@mail.ru>
2022-07-07 10:16:30 +0300

    add interface for working  with key Egais

---
+++ FKCPObj/SimpleClass/IEgais.cs
@@ -0,0 +1,20 @@
+﻿using FKCPObj.SimleClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FKCPObj.SimpleClass
+{
+    public interface IEgais<T> where T : SimpleOP
+    {
+        ulong FsrarID { get; set; }
+        DateOnly Gost { get; set; }
+        DateOnly RSA { get; set; }
+        public IEgais<T> GetEgaisLic();
+        public void SetEgaisLic();
+
+
+    }
+}
