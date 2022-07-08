fa4ba2cfcd7cee96d9790be6231ed280d9df4c38
Дмитрий <deathlukish@mail.ru>
2022-07-08 10:49:16 +0300

    Xmlinterface namespace

---
+++ FKCPObj/XmlInterface/SenderQuery.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FKCPObj.XmlInterface
+{
+    public class SenderQuery
+    {
+        private string resultXML = "";
+
+        public string GetResultXML()
+        {
+            Task task = Task.Run(LoadRefAsync);
+            task.Wait();
+            return resultXML;
+        }
+                
+        /// <summary>
+        /// Отправка запроса на сервер и получения результата в виде XML
+        /// </summary>
+        private async Task LoadRefAsync()
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            string userName = ConfigLoad.GetConfig()?.XMLInterface?.Login;
+            string passwd = ConfigLoad.GetConfig().XMLInterface.Password;
+            byte[] authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+            string url = ConfigLoad.GetConfig().XMLInterface.ServerURL;
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+            using (var client = new HttpClient(httpClientHandler))
+            {
+                using (var multipartFormContent = new MultipartFormDataContent())
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+                    var reqestXML = new StringContent(GetXMLQuery("RESTAURANTS", "Code", "AltName", "DeviceLicenses").ToString());
+                    multipartFormContent.Add(reqestXML);
+                    var response = await client.PostAsync(url, multipartFormContent);
+                    try
+                    {
+                        response.EnsureSuccessStatusCode();
+                        resultXML = await response.Content.ReadAsStringAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Where event
+                        //MessageBox.Show($"Уппс {ex.ToString}");
+                    }
+
+                }
+            }
+ 
+        }
+        /// <summary>
+        /// Метод формироания XML-запроса для сервера
+        /// </summary>
+        /// <param name="RefName">
+        /// Имя справочника в коллекции справочников сервера
+        /// </param>
+        /// <param name="items">
+        /// Список атрибутов для фильтрации на стороне сервера
+        /// Без атрибутов - без фильтрации
+        /// </param>
+        /// <returns>
+        /// XDocument готовый для отправки на сервер
+        /// </returns>
+        private static XDocument GetXMLQuery(string RefName, params string[] items)
+        {
+            XDocument xmlQuery = new XDocument(
+            new XElement("RK7Query",
+                    new XElement("RK7Command2",
+                    new XAttribute("CMD", "GetRefData"),
+                    new XAttribute("RefName", RefName)))
+
+            );
+            if (items.Length != 0)
+            {
+                StringBuilder Prop = new StringBuilder();
+                Prop.AppendJoin(",", items);
+                xmlQuery.Element("RK7Query")?
+                        .Element("RK7Command2")?
+                        .Add(new XAttribute("PropMask", $"items.({Prop})"));
+
+            }
+            return xmlQuery;
+        }
+
+    }
+}
--- Inventar/ViewModels/MainVindowViewModel.cs
+++ Inventar/ViewModels/MainVindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FKCPObj;
+using FKCPObj.XmlInterface;
 using Inventar.Command;
 using Inventar.Views;
 using System;
@@ -27,76 +28,11 @@ namespace Inventar.ViewModels
             OpenConfig = new RelayCommand(OnOpenConfig, CanOpenGonfig);
             BotInit bot = new BotInit(ConfigLoad.GetConfig().ApiToken!.Token!);
             UpdateHandlers.Update += (Message a)=>MessageBox.Show(a.From.ToString());
-            GetRef();
-            //GetXMLQuery("RESTAURANTS", "Code", "AltName", "DeviceLicenses");
-        }
-        /// <summary>
-        /// Отправка запроса на сервер и получения результата в виде XML
-        /// </summary>
-        public async void GetRef()
-        {
-            HttpClientHandler httpClientHandler = new HttpClientHandler();
-            string userName = ConfigLoad.GetConfig().XMLInterface.Login;
-            string passwd = ConfigLoad.GetConfig().XMLInterface.Password;
-            byte[] authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
-            string url = ConfigLoad.GetConfig().XMLInterface.ServerURL;
-            string resultXML;
-            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
-            {
-                return true;
-            };
-            using (var client = new HttpClient(httpClientHandler))
-            {
-                using (var multipartFormContent = new MultipartFormDataContent())
-                {
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
-                    var reqestXML = new StringContent(GetXMLQuery("RESTAURANTS","Code", "AltName", "DeviceLicenses").ToString());
-                    multipartFormContent.Add(reqestXML);
-                    var response = await client.PostAsync(url, multipartFormContent);
-                    try
-                    {
-                        response.EnsureSuccessStatusCode();
-                        resultXML = await response.Content.ReadAsStringAsync();
-                    }
-                    catch(Exception ex)
-                    {
-                        MessageBox.Show($"Уппс {ex.ToString}") ;
-                    }
+            SenderQuery senderQuery = new();
+            string a = senderQuery.GetResultXML();
 
-                }
-            }
         }
-        /// <summary>
-        /// Метод формироания XML-запроса для сервера
-        /// </summary>
-        /// <param name="RefName">
-        /// Имя справочника в коллекции справочников сервера
-        /// </param>
-        /// <param name="items">
-        /// Список атрибутов для фильтрации на стороне сервера
-        /// </param>
-        /// <returns>
-        /// XDocument готовый для отправки на сервер
-        /// </returns>
-        public static XDocument GetXMLQuery(string RefName, params string[] items)
-        {
-            XDocument xmlQuery = new XDocument(
-            new XElement("RK7Query", 
-                    new XElement("RK7Command2", 
-                    new XAttribute("CMD", "GetRefData"),
-                    new XAttribute("RefName", RefName)))
-                
-            );
-            if (items.Length != 0)
-            {
-                StringBuilder Prop = new StringBuilder();
-                Prop.AppendJoin(",", items);              
-                xmlQuery.Element("RK7Query")?
-                        .Element("RK7Command2")?
-                        .Add(new XAttribute("PropMask", $"items.({Prop})"));
 
-            }            
-           return xmlQuery;
-        }
+     
     }
 }
