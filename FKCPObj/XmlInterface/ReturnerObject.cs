a320d477501cc50ae56523c5c74c81f7d97b971a
Дмитрий <deathlukish@mail.ru>
2022-07-12 15:33:54 +0300

    add checking CommandResult

--- FKCPObj/XmlInterface/ReturnerObject.cs
+++ FKCPObj/XmlInterface/ReturnerObject.cs
@@ -7,33 +7,38 @@ namespace FKCPObj.XmlInterface
     {
         public List<SimpleOP>? GetAllOp()
         {
+            List<SimpleOP>? b =  new();
             XmlQueryCreater xmlQuery = new();
             xmlQuery.AddCommand(Rk7Cmd.GetRefData, RefNames.RESTAURANTS, "Code", "AltName", "DeviceLicenses", "Status");
             string a = SenderQuery.GetResultXML(xmlQuery);
-            
             XDocument doc = XDocument.Parse(a);
-            List<SimpleOP>? b = doc.Element("RK7QueryResult")?
+            if (doc.Element("RK7QueryResult")?
                 .Element("CommandResult")?
-                .Element("RK7Reference")?
-                .Element("Items")?
-                .Elements("Item")?
-                .Select(step => new SimpleOP
-                {
-                    Ident = Convert.ToUInt32(step?.Attribute("Ident")?.Value),
-                    AltName = step?.Attribute("AltName")?.Value,
-                    Code = Convert.ToUInt64(step?.Attribute("Code")?.Value),
-                    Status = step?.Attribute("Status")?.Value,
-                    LicenseTxt = step?.Element("DeviceLicenses")?
+                .Attribute("Status")?.Value == "Ok")
+            {
+                     b = doc.Element("RK7QueryResult")?
+                    .Element("CommandResult")?
+                    .Element("RK7Reference")?
                     .Element("Items")?
-                    .Element("Item")?
-                    .Attribute("LicenseTxt")?.Value
-                    .ToString(),
-                    ExpiresAT = step?.Element("DeviceLicenses")?
-                    .Element("Items")?
-                    .Element("Item")?
-                    .Attribute("ExpiresAT")?.Value
-                    .ToString(),
-                })?.ToList();
+                    .Elements("Item")?
+                    .Select(step => new SimpleOP
+                    {
+                        Ident = Convert.ToUInt32(step?.Attribute("Ident")?.Value),
+                        AltName = step?.Attribute("AltName")?.Value,
+                        Code = Convert.ToUInt64(step?.Attribute("Code")?.Value),
+                        Status = step?.Attribute("Status")?.Value,
+                        LicenseTxt = step?.Element("DeviceLicenses")?
+                        .Element("Items")?
+                        .Element("Item")?
+                        .Attribute("LicenseTxt")?.Value
+                        .ToString(),
+                        ExpiresAT = step?.Element("DeviceLicenses")?
+                        .Element("Items")?
+                        .Element("Item")?
+                        .Attribute("ExpiresAT")?.Value
+                        .ToString(),
+                    })?.ToList();
+            }
             return b;
         }
 
@@ -41,11 +46,7 @@ namespace FKCPObj.XmlInterface
         {
             XmlQueryCreater xmlQuery = new();
             xmlQuery.AddCommand(Rk7Cmd.GetRefData, RefNames.CASHES);
-            xmlQuery.AddCommand(Rk7Cmd.GetRefData, RefNames.RESTAURANTS, "Code", "AltName", "DeviceLicenses", "Status");
             string a = SenderQuery.GetResultXML(xmlQuery);
-
-
-            //string a = SenderQuery.GetResultXML(RefNames.CASHES);
             XDocument doc = XDocument.Parse(a);
             List<SimpleUK>? b = doc.Element("RK7QueryResult")?
                 .Element("CommandResult")?
